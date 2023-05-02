5bbda05db1dbca28a4bc55d0dd10fab07b5175db
Jeremy <jeremy@savantly.net>
2023-05-01 22:54:00 -0500

    adjust dollar quoting

--- webapp/src/main/resources/db/migration/POSTGRESQL/_schema.sql
+++ webapp/src/main/resources/db/migration/POSTGRESQL/_schema.sql
@@ -2,4 +2,5 @@ DO $$ BEGIN
     IF to_regtype('causewayExtCommandLog.clob') IS NULL THEN
         create domain causewayExtCommandLog.clob as text;
     END IF;
-END $$;
+END;
+$$  LANGUAGE PLPGSQL;
