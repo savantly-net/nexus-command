6e114c818c660f5f067badd51116023c4a0ac9e0
Jeremy <jeremy@savantly.net>
2024-08-19 01:41:12 -0500

    remove

--- module-flow/src/test/java/net/savantly/nexus/flow/dom/connections/jdbcConnection/JdbcConnectionDestinationHookTest.java
+++ module-flow/src/test/java/net/savantly/nexus/flow/dom/connections/jdbcConnection/JdbcConnectionDestinationHookTest.java
@@ -1,8 +1,6 @@
 package net.savantly.nexus.flow.dom.connections.jdbcConnection;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.sql.Timestamp;
+import java.sql.Time;
 import java.time.ZonedDateTime;
 
 import org.junit.jupiter.api.Test;
@@ -13,6 +11,5 @@ public class JdbcConnectionDestinationHookTest {
     public void testTimestampString() {
         var ts = ZonedDateTime.now();
 
-        Timestamp.valueOf(ts.toString());
     }
 }
